a857839a9def36bfb3987b9557f442ea0f7b95ef
Your Name <you@example.com>
2025-10-24 14:26:17 +0900

    update settings.py files

--- anctl/settings.py
+++ anctl/settings.py
@@ -119,7 +119,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
         'PORT': os.getenv('DB_PORT', '3306'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
