22c1eeec14dc1de75c1c213792adcc554ae43b9b
Your Name <you@example.com>
2025-10-24 14:34:52 +0900

    update settings.py file

--- anctl/settings.py
+++ anctl/settings.py
@@ -116,11 +116,11 @@ WSGI_APPLICATION = 'anctl.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', '3306'),
+        'NAME': os.getenv('DB_NAME', 'railway'),
+        'USER': os.getenv('DB_USER', 'root'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'LjeAxXgvZZtQexdNkwRSULHeAovLpAhY'),
+        'HOST': os.getenv('DB_HOST', 'shortline.proxy.rlwy.net'),
+        'PORT': os.getenv('DB_PORT', '28876'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         },
