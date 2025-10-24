1293863a0fa183eb15cf24a3d7e4f542b46f3542
Your Name <you@example.com>
2025-10-24 11:45:26 +0900

    Edit wsgi.py file

--- anctl/wsgi.py
+++ anctl/wsgi.py
@@ -16,4 +16,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anctl.settings')
 application = get_wsgi_application()
 
 # Set 'app' variable for compatibility with some hosting services
-app = application
+# app = application
